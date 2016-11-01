57a265189cc879e59321046d18c885d443742e98
Niek Mulleners <niekmulleners@gmail.com>
2016-11-01 15:13:20 +0100

    Add 'Bullet'

---
+++ src/Bullet.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE ViewPatterns #-}
+
+module Bullet where
+
+import Graphics.Gloss
+import Graphics.Gloss.Geometry.Angle
+
+import Draw
+import Physics(HasPhysics, Physics, position)
+import qualified Physics
+import Vector (Vector(..))
+import qualified Vector
+
+data Bullet = Bullet
+    { physics :: Physics
+    , direction :: Float
+    } deriving (Show)
+
+instance HasPhysics Bullet where
+    physics a = Bullet.physics a
+    _physics f bullet @ Bullet { physics } =
+        bullet { physics = f physics }
+
+instance Draw Bullet where
+    draw Bullet { physics = position -> Vector x y, direction } =
+        Color white
+            $ Translate x y
+            $ Rotate (radToDeg direction) bullet
+        where
+            bullet = Scale 2 2 $ Line
+                [ (0, 2)
+                , (1, 1)
+                , (1, -1)
+                , (-1, -1)
+                , (-1, 1)
+                , (0, 2)
+                ]
