102d49c855717119a2666839d7eb57355df5c693
Niek Mulleners <niekmulleners@gmail.com>
2016-11-03 16:17:00 +0100

    Add Draw instance for Star

--- src/Model/Star.hs
+++ src/Model/Star.hs
@@ -14,3 +14,15 @@ data Star = Star
 defaultStar :: Star
 defaultStar = Star 0 3
 
+instance Draw Star where
+    draw Star { position = (x, y), depth } = 
+        Color (greyN proximity)
+            $ Translate x y
+            $ Scale size size
+            $ Pictures
+                [ Line [(0, -1), (0, 1)]
+                , Line [(-1, 0), (1, 0)]
+                ]
+                where
+                    size = 10 * proximity
+                    proximity = 1 / fromInteger depth
