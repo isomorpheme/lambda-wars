5ef9fd1a68394c36c2f628511be98caad7d467c3
Daan Rijks <daanrijks@gmail.com>
2016-11-05 23:53:41 +0100

    Document the "Spawn" module

--- src/Spawn.hs
+++ src/Spawn.hs
@@ -5,5 +5,7 @@ import System.Random
 
 import Rectangle
 
+-- | Anything which can be spawned, given the bounds of the screen and
+--   an area to avoid (e.g. around the player).
 class Spawn a where
     spawn :: RandomGen g => Rectangle -> Rectangle -> State g a
