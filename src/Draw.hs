9ff8e5de10bfdb2a496e438d51d2cd065cf67be0
Daan Rijks <daanrijks@gmail.com>
2016-11-05 23:34:41 +0100

    Document the "Draw" module

--- src/Draw.hs
+++ src/Draw.hs
@@ -6,8 +6,10 @@ module Draw where
 import Graphics.Gloss
 import Graphics.Gloss.Geometry.Angle
 
+-- | Anything that can be converted into a 'Picture'
 class Draw a where
     draw :: a -> Picture
 
+-- | Lists of drawables can also be drawn.
 instance Draw a => Draw [a] where
     draw xs = Pictures $ map draw xs
